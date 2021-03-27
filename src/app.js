1652e0da6660734e0da4f4b0530f746b49570d08
Grey Elerson <grey.l.elerson@gmail.com>
2021-03-26 20:17:49 -0600

    add first paths

--- src/app.js
+++ src/app.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const app = express();
 
+const flips = require("./data/flips-data");
+const counts = require("./data/counts-data");
+
 // TODO: Follow instructions in the checkpoint to implement ths API.
+app.use("/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next(`Count type not found: ${countId}`);
+  } else {
+    res.json({data: foundCount})
+  }
+})
+
+app.use("/counts", (req, res, next) => {
+  res.json({data: counts})
+})
+
+app.use("/flips/:id", (req, res, next) => {
+  const { id } = req.params;
+  const flip = flips.find(flip => flip.id === +id);
+
+  if (flip) {
+    res.json({data: flip})
+  } else {
+    next(`Flip id not found: ${id}`);
+  }
+});
+
+app.use("/flips", (req, res, next) => {
+  res.json({data: flips});
+});
 
 // Not found handler
 app.use((request, response, next) => {
