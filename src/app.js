7e019c0569bba2917fffd744cc87351f4304b976
Grey Elerson <grey.l.elerson@gmail.com>
2021-03-28 10:08:16 -0600

    error handler accepts object with status and message

--- src/app.js
+++ src/app.js
@@ -4,9 +4,24 @@ const app = express();
 const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
 
-let lastFlipId = flips.reduce((maxId, flipObj) => Math.max(maxId, flipObj.id), 0)
+let lastFlipId = flips.reduce(
+  (maxId, flipObj) => Math.max(maxId, flipObj.id),
+  0
+);
 
 app.use(express.json());
+
+// New middleware function to validate the request body
+function bodyHasResultProperty(req, res, next) {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    return next(); // Call `next()` without an error message if the result exists
+  }
+  next({
+    status: 400,
+    message: "A 'result' property is required.",
+  });
+}
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
 app.use("/counts", (req, res, next) => {
@@ -17,18 +32,30 @@ app.get("/flips", (req, res, next) => {
   res.json({ data: flips });
 });
 
-app.post("/flips", (req, res, next) => {
+app.post("/flips", bodyHasResultProperty, (req, res, next) => {
   const { data: { result } = {} } = req.body;
-  if (result) {
-    const newFlip = {
-      id: ++lastFlipId, // Increment last id then assign as the current ID
-      result,
-    };
-    flips.push(newFlip);
-    counts[result] = counts[result] + 1; // Increment the counts
-    res.status(201).json({ data: newFlip });
+
+  const newFlip = {
+    id: ++lastFlipId, // Increment last id then assign as the current ID
+    result,
+  };
+
+  flips.push(newFlip);
+  counts[result] = counts[result] + 1; // Increment the counts
+  res.status(201).json({ data: newFlip });
+});
+
+app.get("/flips/:id", (req, res, next) => {
+  const { id } = req.params;
+  const flip = flips.find((flip) => flip.id === +id);
+
+  if (flip) {
+    res.json({ data: flip });
   } else {
-    res.sendStatus(400);
+    next({
+      status: 404,
+      message: `Flip id not found: ${id}`,
+    });
   }
 });
 
@@ -43,26 +70,19 @@ app.use("/:countId", (req, res, next) => {
   }
 });
 
-app.use("/flips/:id", (req, res, next) => {
-  const { id } = req.params;
-  const flip = flips.find((flip) => flip.id === +id);
-
-  if (flip) {
-    res.json({ data: flip });
-  } else {
-    next(`Flip id not found: ${id}`);
-  }
-});
-
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+  next({
+    status: 404,
+    message: `Not found: ${request.originalUrl}`,
+  });
 });
 
 // Error handler
 app.use((error, request, response, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ error: message });
 });
 
 module.exports = app;
